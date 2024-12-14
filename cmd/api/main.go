37a7f2625045d5f79fd98ce2b5844b0906f76578
readyyyk <mr.sairexer@gmail.com>
2024-12-14 16:26:07 +0300

    [F]: validation

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,25 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 
+	env_init "github.com/re-worthy/backend-go/internal/env"
 	"github.com/re-worthy/backend-go/internal/handlers"
 	"github.com/re-worthy/backend-go/pkg/utils"
 )
+
+func init() {
+	env_init.LoadEnv()
+	env_init.ValidateEnv()
+}
 
 func main() {
 	mux := http.NewServeMux()
 	baseHandler := handlers.NewBaseHandler()
 	handlers.SetupRoutes(mux, baseHandler)
 
-	fmt.Println("Server starting on 0.0.0.0:8080...")
+	log.Println("Server starting on 0.0.0.0:8080...")
 	errServer := http.ListenAndServe("0.0.0.0:8080", mux)
 	utils.PanicOnError(&errServer)
 }
